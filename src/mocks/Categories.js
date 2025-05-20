fb5e40b154588e541230de9e9c64fdc1bbd27c8e
Net Jacarei <thaisromero21.tr@gmail.com>
2025-05-20 17:32:19 -0300

    paginas das categorias do delivery

--- src/mocks/Categories.js
+++ src/mocks/Categories.js
@@ -15,3 +15,4 @@ const category = [
 ];
 
 export default category;
+
---
+++ src/mocks/PageCategory.js
@@ -0,0 +1,40 @@
+let pageCategory = [
+    
+ {
+    category: "carnes",
+	title: "Carnes",
+	"description": "Sabores irresistíveis para os amantes de carne.",
+ },
+
+  {
+    category: "mar",
+	title: "Do Mar",
+	description: "Pratos refinados com frutos do mar frescos, preparados com técnica e criatividade para uma experiência única e sofisticada.",
+ },
+
+  {
+    category: "vegetariano",
+	title: "Vegetariano",
+	description: "Receitas criativas e sofisticadas que destacam o frescor e o sabor dos vegetais, oferecendo uma experiência leve, colorida e surpreendente.",
+ },
+
+  {
+    category: "sobremesas",
+	title: "Sobremesas",
+	description: "Sobremesas elegantes e irresistíveis, que combinam técnica e delicadeza para encerrar sua experiência com doçura e sofisticação.",
+ },
+
+ {
+    category: "sem alcool",
+	title: "Bebidas sem alcool",
+	description: "Bebidas não alcoólicas criativas e refrescantes, elaboradas com frutas, ervas e especiarias para equilibrar sabor, leveza e prazer em cada gole.",
+ },
+
+ {
+    category: " com alcool",
+	title: "Bebidas sem alcool",
+	description: "Drinks autorais e clássicos preparados com destilados premium, ingredientes selecionados e um toque de ousadia para brindar com estilo e personalidade.",
+ },
+]
+
+export default pageCategory;
